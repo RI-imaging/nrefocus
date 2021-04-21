4e666d333eb17a762d914ff4bcb4191348f15afc
Paul Müller <dev@craban.de>
2021-04-21 23:54:29 +0200

    dist: include submodules in wheel/dist

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from os.path import exists, dirname, realpath
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys
 
 
@@ -19,13 +19,13 @@ setup(
     author_email="dev@craban.de",
     url="https://github.com/RI-imaging/nrefocus",
     version=version,
-    packages=[name],
+    packages=find_packages(),
     package_dir={name: name},
     license="BSD (3 clause)",
     description=description,
     long_description=open("README.rst").read() if exists("README.rst") else "",
     install_requires=["numpy>=1.5.1"],
-    python_requires=">=3.4, <4",
+    python_requires=">=3.6, <4",
     keywords=["autofocus",
               "refocus",
               "numerical focusing",

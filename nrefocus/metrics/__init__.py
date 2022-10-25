1d76801721ea6b67a1404efcfbcc7babc006aef4
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2022-10-25 13:57:41 +0200

    feat: standard deviation metric added

--- nrefocus/metrics/__init__.py
+++ nrefocus/metrics/__init__.py
@@ -1,6 +1,7 @@
 from .mt_avg_grad import metric_average_gradient
 from .mt_rms_contrast import metric_rms_contrast
 from .mt_spectrum import metric_spectrum
+from .mt_std_grad import metric_std_gradient
 
 
 #: Available metrics
@@ -8,4 +9,5 @@ METRICS = {
     "average gradient": metric_average_gradient,
     "rms contrast": metric_rms_contrast,
     "spectrum": metric_spectrum,
+    "std gradient": metric_std_gradient,
 }

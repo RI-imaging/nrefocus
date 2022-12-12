9708330d991d33cec66fb597e9140b4c4de5848d
Eoghan O'Connell <eoghan.oconnell@mpl.mpg.de>
2022-12-12 12:13:50 +0100

    docs: include a metric comparison example in the docs

--- examples/compare_metrics.py
+++ examples/compare_metrics.py
@@ -1,19 +1,27 @@
+"""Compare available Metrics for Refocusing
+
+The HL60 cell data used is described in `refocus_cell.py`.
+"""
+
 import nrefocus
 from nrefocus.metrics import METRICS
 import matplotlib.pyplot as plt
 
 from examples.example_helper import load_cell
 
-
+# load initial cell and create rf object
 rf = nrefocus.iface.RefocusNumpy(field=load_cell("HL60_field.zip"),
                                  wavelength=647e-9,
                                  pixel_size=0.139e-6,
                                  kernel="helmholtz")
 
+# get a list of all available metrics
 my_metrics = list(METRICS.keys())
 
+# plot results
 fig, axes = plt.subplots(1, len(my_metrics), figsize=(12, 5))
 
+# autofocus the image for each metric
 for i, mt in enumerate(my_metrics):
     af_vals = rf.autofocus(metric=mt,
                            minimizer="lmfit",
